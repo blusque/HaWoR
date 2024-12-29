7216cda810a4e951ab22375026a9e1932bb69ed7
ThunderVVV <zhangjinglei168@sjtu.edu.cn>
2024-12-29 21:26:18 +0800

    update

--- demo.py
+++ demo.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
             os.makedirs(output_pth)
         image_names = imgfiles[vis_start:vis_end]
         print(f"vis {vis_start} to {vis_end}")
-        run_vis2_on_video_cam(left_dict, right_dict, output_pth, img_focal, image_names, R_c2w=R_w2c_sla_all[vis_start:vis_end], t_c2w=t_w2c_sla_all[vis_start:vis_end])
+        run_vis2_on_video_cam(left_dict, right_dict, output_pth, img_focal, image_names, R_w2c=R_w2c_sla_all[vis_start:vis_end], t_w2c=t_w2c_sla_all[vis_start:vis_end])
 
     print("finish")
 
